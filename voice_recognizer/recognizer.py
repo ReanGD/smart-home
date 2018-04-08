670baf68f61b606213a490a974ec8c8abe964aa7
ReanGD <reangd@yandex.ru>
2018-04-08 21:09:48 +0300

    refactor recognizer

--- voice_recognizer/recognizer.py
+++ voice_recognizer/recognizer.py
@@ -19,20 +19,37 @@ class Recognizer(object):
         else:
             self._hotword_detector = self._snowboy
 
+        # params
+        timeout_before_phrase_ms = 3 * 1000
+        timeout_after_phrase_ms = 2 * 1000
+        timeout_speech_detect_ms = 500
+        speech_detect_ratio = 0.9
+        self._time_read_ms = 40
+
+        # tmp values
+        self._timeout_before_phrase_periods = int(timeout_before_phrase_ms / self._time_read_ms)
+        self._timeout_after_phrase_periods = int(timeout_after_phrase_ms / self._time_read_ms)
+        self._speech_detect_buffer_maxlen = int(timeout_speech_detect_ms / self._time_read_ms)
+        self._speech_detect_threshold = speech_detect_ratio * self._speech_detect_buffer_maxlen
+
     def get_audio_settings(self,
                            device: Device,
                            device_index=None,
                            frames_per_buffer=2048) -> StreamSettings:
         return self._snowboy.get_audio_settings(device, device_index, frames_per_buffer)
 
+    def _calc_frames_read(self, stream: Stream):
+        frames_read = stream.get_settings().get_frames_count_by_duration_ms(self._time_read_ms)
+        msg = 'Invalid value "frames_per_buffer" in stream.settings'
+        assert frames_read <= stream.get_settings().frames_per_buffer, msg
+
+        return frames_read
+
     def wait_hotword(self, stream: Stream):
-        settings = stream.get_settings()
-        period_ms = 40
-        frames_cnt = settings.get_frames_count_by_duration_ms(period_ms)
-        assert frames_cnt <= settings.frames_per_buffer, "Invalid frames_per_buffer in settings"
+        frames_read = self._calc_frames_read(stream)
 
         while True:
-            frames = stream.read(frames_cnt)
+            frames = stream.read(frames_read)
             if len(frames) == 0:
                 return False
 
@@ -40,42 +57,56 @@ class Recognizer(object):
                 return True
 
     def read_phrase(self, stream: Stream, timeout_sec=20) -> AudioData:
-        settings = stream.get_settings()
-        period_ms = 20
-        frames_cnt = settings.get_frames_count_by_duration_ms(period_ms)
-        assert frames_cnt <= settings.frames_per_buffer, "Invalid frames_per_buffer in settings"
+        timeout_periods = int(timeout_sec * 1000.0 / self._time_read_ms)
+        assert timeout_periods > self._timeout_before_phrase_periods, 'Invalid arg "timeout_sec"'
 
+        settings = stream.get_settings()
+        frames_read = self._calc_frames_read(stream)
         voice = []
-        silent_cnt = 0
-        silent_max = int(2 * 1000.0 / period_ms)
-        state_active = False
-        ring_buffer = collections.deque(maxlen=30)
-        for _ in range(0, int(timeout_sec * 1000.0 / period_ms)):
-            frames = stream.read(frames_cnt)
+        speech_detect_buffer = collections.deque(maxlen=self._speech_detect_buffer_maxlen)
+        state_speech = False
+        phrase_started = False
+        state_cnt = 0
+
+        for _ in range(timeout_periods):
+            frames = stream.read(frames_read)
             if len(frames) == 0:
                 return None
 
             voice.append(frames)
-            is_speech = self._snowboy.is_speech(frames)
-            ring_buffer.append(is_speech)
+            speech_detect_buffer.append(self._snowboy.is_speech(frames))
+            state_cnt += 1
 
-            if not state_active:
-                num_voiced = len([1 for speech in ring_buffer if speech])
-                if num_voiced > 0.9 * len(ring_buffer):
-                    state_active = True
+            if len(speech_detect_buffer) < speech_detect_buffer.maxlen:
+                continue
+
+            if not state_speech:
+                if speech_detect_buffer.count(True) > self._speech_detect_threshold:
+                    if not phrase_started:
+                        phrase_started = True
+                        voice = voice[state_cnt - speech_detect_buffer.maxlen:]
+                    state_cnt = 1
+                    state_speech = True
             else:
-                num_unvoiced = len([1 for speech in ring_buffer if not speech])
-                if num_unvoiced > 0.9 * len(ring_buffer):
-                    state_active = False
+                if speech_detect_buffer.count(False) > self._speech_detect_threshold:
+                    state_cnt = 1
+                    state_speech = False
 
-            if state_active:
-                silent_cnt = 0
-            else:
-                silent_cnt += 1
-                if silent_cnt > silent_max:
-                    break
+            # timeout before phrase
+            if not phrase_started and self._timeout_before_phrase_periods <= state_cnt:
+                return None
 
-        return AudioData(b''.join(voice[:-silent_max]), stream.get_settings())
+            # timeout after phrase
+            if (phrase_started
+                    and not state_speech
+                    and self._timeout_after_phrase_periods <= state_cnt):
+                return AudioData(b''.join(voice[:-state_cnt]), settings)
+
+        # total timeout
+        if state_speech:
+            return AudioData(b''.join(voice), settings)
+        else:
+            return AudioData(b''.join(voice[:-state_cnt]), settings)
 
     def recognize(self, audio_date: AudioData):
         return self._recognizer.recognize(audio_date)
