591399260699a19a7b7962c2a3da8b5411610a2b
ReanGD <reangd@yandex.ru>
2018-05-01 14:41:52 +0300

    unlimit record example

--- experiments/record.py
+++ experiments/record.py
@@ -2,19 +2,19 @@ import audio
 
 
 def run():
-    seconds = 15
-
     device = audio.Device()
+    settings = audio.StreamSettings(device, device_index=None)
+    data = []
     try:
-        settings = audio.StreamSettings(device, device_index=None)
         print("settings: {}".format(settings))
         mic = device.create_microphone_stream(settings)
-
-        step_cnt = int(settings.sample_rate / settings.frames_per_buffer * seconds)
         print("start record...")
-        data = b''.join([mic.read(settings.frames_per_buffer) for _ in range(step_cnt)])
-        print("stop record...")
 
-        audio.AudioData(data, settings).save_as_wav("record.wav")
+        while True:
+            data.append(mic.read(settings.frames_per_buffer))
+
+    except KeyboardInterrupt:
+        print("stop record...")
+        audio.AudioData(b''.join(data), settings).save_as_wav("record.wav")
     finally:
         device.terminate()
