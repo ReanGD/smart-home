9eaca8ed27cdd0c6d5ed7a77e455e7c41dd38783
ReanGD <reangd@yandex.ru>
2018-05-02 00:35:18 +0300

    split file customization

--- experiments/split_file.py
+++ experiments/split_file.py
@@ -25,9 +25,20 @@ def run():
         indata = audio.AudioData.load_as_wav(in_path, settings)
         raw = indata.get_raw_data()
 
-        for ind, it in enumerate(load_labels(labels_path)):
+        # for ind, it in enumerate(load_labels(labels_path)):
+        #     start = it[0] * settings.sample_width
+        #     stop = it[1] * settings.sample_width
+        #     out_path = os.path.join(out_dir, "{}.wav".format(ind))
+        #     audio.AudioData(raw[start:stop], settings).save_as_wav(out_path)
+
+        labels = load_labels(labels_path)
+        ranges = [[labels[i-1][0], labels[i][0]] for i in range(1, len(labels))]
+        ranges.append([labels[-1][0], len(raw) // settings.sample_width])
+
+        for ind, it in enumerate(ranges):
             start = it[0] * settings.sample_width
             stop = it[1] * settings.sample_width
+            print(start, stop)
             out_path = os.path.join(out_dir, "{}.wav".format(ind))
             audio.AudioData(raw[start:stop], settings).save_as_wav(out_path)
 
