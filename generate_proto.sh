335f508f8a04825ec51b9a27fd1a392c9dbd2edf
ReanGD <reangd@yandex.ru>
2018-06-23 01:15:25 +0300

    Add HA protobuf

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 protoc --proto_path=recognition/yandex_api --python_out=recognition/yandex_api recognition/yandex_api/*.proto
+protoc --proto_path=protocols/home_assistant --python_out=protocols/home_assistant protocols/home_assistant/*.proto
---
+++ protocols/home_assistant/__init__.py
@@ -0,0 +1,7 @@
+from . import protocol_pb2
+
+from .protocol_pb2 import StartRecognition, SetDeviceState
+
+__all__ = ['StartRecognition',
+           'SetDeviceState',
+           ]
