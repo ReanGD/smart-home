b44c59817464bd5bd0a6c1e4b2bcb9c0aa87df6c
ReanGD <reangd@yandex.ru>
2018-08-12 21:11:24 +0300

    move yandex proto files

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-protoc --proto_path=recognition/yandex_api --python_out=recognition/yandex_api recognition/yandex_api/*.proto
+protoc --proto_path=protocols/yandex         --python_out=protocols/yandex         protocols/yandex/*.proto
 protoc --proto_path=protocols/home_assistant --python_out=protocols/home_assistant protocols/home_assistant/*.proto
-protoc --proto_path=test/test_proto --python_out=test/test_proto test/test_proto/*.proto
+protoc --proto_path=test/test_proto          --python_out=test/test_proto          test/test_proto/*.proto
