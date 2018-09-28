2c817797e18cf64e291c38d07bb382bc247a5cd6
ReanGD <reangd@yandex.ru>
2018-09-28 03:22:08 +0300

    generate proto only for exists files

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-protoc --proto_path=protocols/yandex         --python_out=protocols/yandex         protocols/yandex/*.proto
-protoc --proto_path=protocols/home_assistant --python_out=protocols/home_assistant protocols/home_assistant/*.proto
-protoc --proto_path=test/test_proto          --python_out=test/test_proto          test/test_proto/*.proto
+if [ -d "$ROOT_DIR/protocols/yandex" ]; then
+	protoc --proto_path=$ROOT_DIR/protocols/yandex         --python_out=$ROOT_DIR/protocols/yandex         $ROOT_DIR/protocols/yandex/*.proto
+fi
+if [ -d "$ROOT_DIR/protocols/home_assistant" ]; then
+    protoc --proto_path=$ROOT_DIR/protocols/home_assistant --python_out=$ROOT_DIR/protocols/home_assistant $ROOT_DIR/protocols/home_assistant/*.proto
+fi
+if [ -d "$ROOT_DIR/test/test_proto" ]; then
+    protoc --proto_path=$ROOT_DIR/test/test_proto          --python_out=$ROOT_DIR/test/test_proto          $ROOT_DIR/test/test_proto/*.proto
+fi
