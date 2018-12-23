7a56c2c5edab29acd70e15855fdf60237d45ab14
ReanGD <reangd@yandex.ru>
2018-12-23 16:28:05 +0300

    independent command class for hass

---
+++ home_assistant/commands.py
@@ -0,0 +1,87 @@
+from logging import Logger
+from typing import Optional
+from homeassistant.core import HomeAssistant
+from nlp import Morphology
+from etc import all_entitis
+
+
+class HassCommands:
+    def __init__(self, logger: Logger, hass: HomeAssistant, default_place: str):
+        self._hass = hass
+        self._logger = logger
+        self._morph = Morphology(all_entitis)
+        self._default_place = default_place
+
+    def _check_device(self, device: Optional[str]) -> bool:
+        if device is None:
+            self._logger.error('Unknown "device": None')
+            return False
+
+        if device not in ['light', 'tv', 'music']:
+            self._logger.error('Unknown "device": {}'.format(device))
+            return False
+
+        if device != 'light':
+            self._logger.error('Unsupported "device": {}'.format(device))
+            return False
+
+        return True
+
+    def _check_device_action(self, device_action: Optional[str]) -> bool:
+        if device_action is None:
+            self._logger.error('Unknown "device_action": None')
+            return False
+
+        if device_action not in ['turn_off', 'turn_on']:
+            self._logger.error('Unknown "device_action": {}'.format(device_action))
+            return False
+
+        return True
+
+    def _check_place(self, place: Optional[str]):
+        if place is None:
+            self._logger.error('Unknown "place": None')
+            return False
+
+        if place not in ['hall', 'kitchen', 'restroom', 'bathroom', 'livingroom', 'playroom',
+                         'all', 'here']:
+            self._logger.error('Unknown "place": {}'.format(place))
+            return False
+
+        return True
+
+    def _make_name(self, device: str, device_action: str, place: str) -> str:
+        if device_action == 'turn_off':
+            device_action_str = 'off'
+        else:
+            device_action_str = 'on'
+
+        if place == 'here':
+            place_str = self._default_place
+        else:
+            place_str = place
+
+        result = '{}_{}_{}'.format(place_str, device, device_action_str)
+        return result
+
+    async def execute(self, message: str) -> bool:
+        cmd = self._morph.analyze(message)
+
+        device = cmd.get('device', None)
+        device_action = cmd.get('device_action', None)
+        place = cmd.get('place', None)
+
+        if not self._check_device(device):
+            return False
+
+        if not self._check_device_action(device_action):
+            return False
+
+        if not self._check_place(place):
+            return False
+
+        service_data = {}
+        name = self._make_name(device, device_action, place)
+        await self._hass.services.async_call('script', name, service_data, False)
+
+        return True
--- home_assistant/server.py
+++ home_assistant/server.py
@@ -1,66 +1,20 @@
 from logging import Logger, getLogger
 from homeassistant.core import HomeAssistant
-from nlp import Morphology
-from etc import HassTransportConfig, all_entitis
+from etc import HassTransportConfig
 from protocols.transport import TCPServerConnection, TCPServer
 from protocols.home_assistant import (HASerrializeProtocol, StartRecognition,
                                       UserTextCommand, UserTextCommandResult)
+from .commands import HassCommands
 
 
 class HomeAssistentConnection(TCPServerConnection):
     def __init__(self, logger: Logger, hass: HomeAssistant):
         super().__init__(logger)
-        self._hass = hass
-        self._morph = Morphology(all_entitis)
-
-    async def _run_command(self, cmd):
-        self._logger.info('Recognized the command: {}'.format(cmd))
-
-        device_action = cmd['device_action']
-        device = cmd['device']
-        place = cmd['place']
-
-        if device not in ['light', 'tv', 'music']:
-            self._logger.error('Unknown "device": {}'.format(device))
-            return False
-
-        if device != 'light':
-            self._logger.error('Unsupported "device": {}'.format(device))
-            return False
-
-        if device_action not in ['turn_off', 'turn_on']:
-            self._logger.error('Unknown "device_action": {}'.format(device_action))
-            return False
-
-        if device_action == 'turn_off':
-            device_action_str = 'off'
-        else:
-            device_action_str = 'on'
-
-        if place not in ['hall', 'kitchen', 'restroom', 'bathroom', 'livingroom', 'playroom',
-                         'all', 'here']:
-            self._logger.error('Unknown "place": {} for "device_action": {}'.format(
-                place, device_action))
-            return False
-
-        if place == 'here':
-            place_str = 'livingroom'
-        else:
-            place_str = place
-
-        service = '{}_{}_{}'.format(place_str, device, device_action_str)
-        service_data = {}
-        await self._hass.services.async_call('script', service, service_data, False)
-
-        return True
+        self._commands = HassCommands(logger, hass, 'livingroom')
 
     async def on_user_text_command(self, message: UserTextCommand):
-        cmd = self._morph.analyze(message.text)
-        isFinished = 'place' in cmd and 'device' in cmd and 'device_action' in cmd
-        if isFinished:
-            isFinished = await self._run_command(cmd)
-
-        await self.send(UserTextCommandResult(isFinished=isFinished))
+        is_finished = self._commands.execute(message.text)
+        await self.send(UserTextCommandResult(isFinished=is_finished))
 
 
 class HomeAssistentServer(object):
